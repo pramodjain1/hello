084e3f28f320ae77e606d2cf1630dedc5ef6f273
pramodjain1 <pramod.jain@outlook.com>
2025-07-04 18:15:35 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const launchDateStr = process.env.REACT_APP_LAUNCH_DATE;
+
+  const launchDate = new Date(launchDateStr);
+  const today = new Date();
+  const daysRemaining = Math.ceil((launchDate - today) / (1000 * 60 * 60 * 24));
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>COMING SOON</p>
+        {launchDateStr && (
+          <div style={{ padding: '2rem', fontSize: '1.5rem' }}>
+            🚀 Launch Date: <strong>{launchDateStr}</strong>
+            <br />⏳ Days Remaining: <strong>{daysRemaining}</strong>
+          </div>
+        )}
       </header>
     </div>
   );
